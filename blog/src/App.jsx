84f38145e7654f7b26e1c460f7c6f47fdec5bda5
print01python <iantoniojlopezz@gmail.com>
2023-11-26 23:15:36 +0100

    started pack

---
+++ blog/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div></div>
+      <p className="read-the-docs">started pack</p>
+    </>
+  );
+};
+
+export default App;
